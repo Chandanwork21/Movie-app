277bada1d7fc0443d80decfe3b99cd79d819090d
chandan yadav <yadavchandanr123@gmail.com>
2025-03-08 21:23:23 +0530

    vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MovieSearch from './components/MovieSearch'
 import Favorites from './components/Favourites';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react"
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
      <Route path="/favorites" element={<Favorites />} />
       </Routes>
     </Router>
+    <Analytics />
     </>
   )
 }
